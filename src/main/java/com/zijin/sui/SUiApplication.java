afdb12371c47003a40a0f547856bf634495d83c3
Eliauk365 <57357340+Eliauk365@users.noreply.github.com>
2023-05-27 22:20:24 +0800

    Creat Project

---
+++ src/main/java/com/zijin/sui/SUiApplication.java
@@ -0,0 +1,13 @@
+package com.zijin.sui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zijin/sui/SUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zijin.sui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
